6758d80611c786608201ec871031b990251cc897
au <nakayamaeiyu@gmail.com>
2026-06-22 14:27:49 +0900

    Add user input validation for login

--- index/login.js
+++ index/login.js
@@ -1,1 +1,19 @@
+const emailList = ["723108@kng.ed.jp"]
+const passwordList = ["111"]
 
+function validateUserInput() {
+    const inputName = document.getElementById('usernameInput').value
+    const inputPassword = document.getElementById('passwordInput').value
+
+    if (emailList.includes(inputName)) {
+        const emailTag = emailList.indexOf(inputName)
+        if (inputPassword === passwordList[emailTag]) {
+            window.location.href = "../src/pages/dashboard.html"
+            alert("Login successful.")
+        } else {
+            alert("Password is incorrect.")
+        }
+    } else {
+        alert("Username does not exist.")
+    }
+}
