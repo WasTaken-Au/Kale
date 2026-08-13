85a1facc82485e6bd0a07e1497f9b96aa2fe7815
au <nakayamaeiyu@gmail.com>
2026-08-13 23:49:25 +0900

    Refactor coursePageRender to establishPage and goToHome

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,3 +1,16 @@
-function coursePageRender(chapter) {
-    
+function establishPage(topic) {
+    if (confirm("Do you want to start this topic?") == true) {
+        localStorage.setItem('topicNo',topic)
+        window.location.href='course.html'
+        let guideNo
+        guideNo = 0
+    }
+}
+
+function goToHome(page) {
+    if (page == 'course' && confirm("Do you want to leave topic?\nProgress will not be saved.")) {
+        window.location.href='home.html'
+    } else {
+        window.location.href='home.html'
+    }
 }
